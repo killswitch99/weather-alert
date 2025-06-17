5a48f6b55185ea3c1fd3b4c721466800fe2ee6ea
topuahmed <atopu95@gmail.com>
2025-06-17 11:28:24 +1000

    feat: implement JsonMiddleware to set Content-Type to application/json

---
+++ api/pkg/api/middleware/middleware.go
@@ -0,0 +1,11 @@
+package middleware
+
+import "net/http"
+
+// JsonMiddleware sets the Content-Type header to application/json
+func JsonMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
+}
